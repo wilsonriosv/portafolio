d84c7a9add7330b71ab9eec66d9308685db2b002
wilsonriosv <wilsonriosv@gmail.com>
2024-07-02 15:43:03 -0500

    Template listo sin rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { PortafolioComponent } from "./pages/portafolio/portafolio.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, PortafolioComponent]
 })
 export class AppComponent {
   title = 'portafolio';
---
+++ src/app/pages/portafolio/portafolio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portafolio',
+  standalone: true,
+  imports: [],
+  templateUrl: './portafolio.component.html',
+  styleUrl: './portafolio.component.css'
+})
+export class PortafolioComponent {
+
+}
