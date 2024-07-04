654d534b3948107af8c2bb3c2d9e173f60eaa734
wilsonriosv <wilsonriosv@gmail.com>
2024-07-03 19:22:12 -0500

    Implementacion de about

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from '../../services/info-pagina.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit{
+
+  constructor( public infoPaginaService: InfoPaginaService){}
+
+  ngOnInit(): void {
+      
+  } 
 
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,17 +8,31 @@ import { IInfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: IInfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) { 
     console.log("Info servicio de infoPagina listo")
     
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     //Leer el archivo JSON
     this.http.get('assets/data/data-paginas.json')
     .subscribe((data:IInfoPagina) => {
       this.cargada = true;
       this.info = data;
-      console.log(this.info);
+      // console.log(this.info);
     });
   }
   
+  cargarEquipo(){
+    //Leer el archivo JSON
+    this.http.get('https://angular-html-e124f-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe((data:any) => {
+      this.equipo = data;
+      console.log(this.equipo);
+    });
+  }
 }
